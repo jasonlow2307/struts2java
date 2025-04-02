6839899ddabe805dfd6239e45f07781e5894574c
Jason <jason.lowjx@gmail.com>
2025-04-02 09:48:07 +0800

    chore: added CORS

--- src/main/java/com/deloitte/struts2java/controller/LoginController.java
+++ src/main/java/com/deloitte/struts2java/controller/LoginController.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
