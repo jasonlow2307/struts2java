c756e2d58c8a3f73597f274f6f0f86ae7a24f1e5
Chen Wei Lun <weilunchen210@gmail.com>
2025-04-03 15:27:55 +0800

    adjusted id from integer to long

--- src/main/java/com/deloitte/models/Item.java
+++ src/main/java/com/deloitte/models/Item.java
@@ -1,36 +1,37 @@
 package com.deloitte.models;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.DecimalMin;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.validation.constraints.DecimalMin;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Entity
+@Table(name = "items")
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Item {
-    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
     @NotBlank(message = "Name is required")
+    @Column(nullable = false)
     private String name;
-    
+
+    @NotBlank(message = "Description is required")
+    @Column(nullable = false)
     private String description;
-    
+
     @NotNull(message = "Price is required")
-    @DecimalMin(value = "0.0", inclusive = false, message = "Price must be greater than 0")
+    @DecimalMin(value = "0.0", inclusive = true, message = "Price must be non-negative")
+    @Column(nullable = false)
     private BigDecimal price;
 }
